79c6222218d6ba0c2a2613f4eb36d2041fc804f7
stheves <sascha@theves.io>
2019-12-14 16:25:32 +0100

    react as peer dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader'],
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: 'react-pairs-game.js',
-        library: 'ReactPairsGame',
-        libraryTarget: 'umd',
-    },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDom',
-    },
-    devServer: {
-        contentBase: [__dirname + '/dist', __dirname + '/demo'],
-    },
+   entry: './src/index.js',
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader', 'eslint-loader'],
+         },
+         {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: ['style-loader', 'css-loader'],
+         },
+      ],
+   },
+   output: {
+      path: __dirname + '/dist',
+      filename: 'react-pairs-game.js',
+      library: 'ReactPairsGame',
+      libraryTarget: 'umd',
+   },
+   externals: {
+      react: 'React',
+      'react-dom': 'ReactDom',
+   },
+   devServer: {
+      contentBase: [__dirname + '/dist', __dirname + '/demo'],
+   },
 };
