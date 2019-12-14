2a327fc4072ea6e63507c313fb30b90518003ad4
stheves <sascha@theves.io>
2019-12-14 11:59:08 +0100

    eject react app, use plain webpack/babel config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'react-pairs-game.js'
+    },
+    devServer: {
+        contentBase: __dirname + '/dist'
+    }
+};
