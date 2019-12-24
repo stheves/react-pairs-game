1f20f78fedcfba072260de93e6dcc805e0d173b0
stheves <sascha@theves.io>
2019-12-24 13:39:26 +0100

    create drop down menu for main navbar

--- src/board/Menu.js
+++ src/board/Menu.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Menu.css';
 import PropTypes from 'prop-types';
 
 const Menu = ({ onStartClick }) => {
-   return (
+   const [dropdown, setDropdown] = useState(false);
+
+   function toggleDropDown() {
+      setDropdown(!dropdown);
+   }
+
+    function handleStartClick() {
+        setDropdown(false);
+        onStartClick();
+    }
+
+    return (
       <nav className={'game-navbar'}>
          <ul className={'game-navbar-nav'}>
-            <li className={'game-navbar-item'}>
-               <a href={'#'} className={'game-nav-link'} onClick={onStartClick}>
+            <li className="game-navbar-item dropdown">
+               <a
+                  href={'#'}
+                  className={'game-nav-link'}
+                  onClick={toggleDropDown}
+               >
                   Start
                </a>
+               <div className={'dropdown-menu ' + (dropdown ? 'visible' : '')}>
+                  <a className="dropdown-item" href="#" onClick={handleStartClick}>
+                     New Game
+                  </a>
+               </div>
             </li>
          </ul>
       </nav>
@@ -18,10 +38,6 @@ const Menu = ({ onStartClick }) => {
 
 Menu.propTypes = {
    onStartClick: PropTypes.func,
-};
-
-Menu.defaultProps = {
-   onStartClick: () => {},
 };
 
 export default Menu;
